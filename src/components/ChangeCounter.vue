313e629e51ea09b99979aeb91c7d8dfe913cc34a
RenataNorkiene <renata.norkiene@gmail.com>
2022-01-11 16:51:00 +0200

    all files organized in other struture: modules, index, getters etc..

--- src/components/ChangeCounter.vue
+++ src/components/ChangeCounter.vue
@@ -12,7 +12,7 @@ export default {
         // addOne(){
         //     this.$store.dispatch('increment');
         // }
-        ...mapActions({
+        ...mapActions('numbers', {
             inc: 'increment',
             increase: 'increase'
         })
--- src/main.js
+++ src/main.js
@@ -1,63 +1,9 @@
 import { createApp } from 'vue';
-import { createStore } from 'vuex'
+import store from "@/store/index";
 
 import App from './App.vue';
 
 const app = createApp(App);
-
-const store = createStore({
-    state() {
-        return {
-            counter: 0,
-            isLoggedIn: false,
-        };
-    },
-    mutations: {
-        increment(state) {
-            state.counter = state.counter + 2;
-        },
-        increase(state, payload) {
-            state.counter = state.counter + payload.value;
-        },
-        setAuth(state, payload) {
-            state.isLoggedIn = payload.isAuth;
-        }
-    },
-    actions: {
-        increment(context) {
-            setTimeout(function () {
-                context.commit('increment');
-            }, 2000);
-        },
-        increase(context, payload) {
-            context.commit('increase', payload);
-        },
-        logIn(context) {
-            context.commit('setAuth', { isAuth: true });
-        },
-        logOut(context) {
-            context.commit('setAuth', { isAuth: false });
-        },
-    },
-    getters: {
-        finalCounter(state) {
-            return state.counter * 3;
-        },
-        normalizedCounter(_, getters) {
-            const finalCounter = getters.finalCounter;
-            if (finalCounter < 0) {
-                return 0;
-            }
-            if (finalCounter > 100) {
-                return 100;
-            }
-            return finalCounter;
-        },
-        userIsAuthenticated(state) {
-            return state.isLoggedIn;
-        },
-    },
-});
 
 app.use(store);
 
